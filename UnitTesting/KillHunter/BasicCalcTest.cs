e5ccb934541463465e2d6e180e629309c9630da2
0109-13 <0109-13@0109-13>
2024-10-04 14:15:05 +0300

    Р”РѕР±Р°РІР»РµРЅ РЅРѕРІС‹Р№ РєР»Р°СЃСЃ

---
+++ UnitTesting/KillHunter/BasicCalcTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingLib.Math;
+
+namespace UnitTesting.KillHunter
+{
+    public class BasicCalcTest
+    {
+        private readonly BasicCalc _isPerfectNum;
+
+        public BasicCalcTest()
+        { 
+            _isPerfectNum = new BasicCalc();
+        }
+
+        [Fact]
+        public void IsPerfectNumber_MustReturnTrue()
+        {
+            bool result = _isPerfectNum.IsPerfectNumber(6);
+            Assert.True(result);
+        }
+        [Fact]
+        public void IsPerfectNumber_MustReturnFalse()
+        {
+            bool result = _isPerfectNum.IsPerfectNumber(8);
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsPerfectNumber_IncorrectData()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(()=>_isPerfectNum.IsPerfectNumber(-5));
+        }
+
+        [Theory]
+        [InlineData(6, true)]
+        [InlineData(8, false)]
+        public void IsPerfectNumber_Theory(int n, bool expectedResult)
+        {
+            bool result = _isPerfectNum.IsPerfectNumber(n);
+            Assert.Equal(expectedResult,result);
+        }
+
+    }
+}
